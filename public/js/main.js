7ad7617df9320238abbd0bad403bb870f2372ce7
0wx <callmeoniichandesu@gmail.com>
2020-05-31 03:44:10 +0700

    fix js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 function getHost() {
     var x = 'http://' + location.hostname + '/https://yourblockedurl.com/image.png';
     document.getElementById("host").innerHTML= x;
+    document.getElementById("host2").innerHTML= x;
 }
